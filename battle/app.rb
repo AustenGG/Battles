90f2116d652c8440ed5283580e1a46ea627bf60a
Makers Academy <onboarding@makersacademy.com>
2018-10-18 08:47:49 +0000

    enter names

--- battle/app.rb
+++ battle/app.rb
@@ -16,5 +16,15 @@ class Battle < Sinatra::Base
 
   end
 
-  
+  post '/names' do
+
+    session[:player_1_name] = params[:player_1_name]  # takes users names as an input then redirects to play.
+
+    session[:player_2_name] = params[:player_2_name]
+
+    redirect '/play'
+
+  end
+
+
 end
